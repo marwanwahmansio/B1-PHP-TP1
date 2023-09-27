26a47d80f8f01d61aadf326d7900f44f68aa1fe8
marwanwahmansio <144268508+marwanwahmansio@users.noreply.github.com>
2023-09-27 16:58:24 +0200

    Add files via upload

--- tarifReduitGroupe.php
+++ tarifReduitGroupe.php
@@ -1,11 +1,10 @@
 <?php
-	define("TarifPlein",18) ;
-	define("TarifReduit",15) ;
 	
+
 	echo "Combien y'a t'il d'adultes ?:\n" ;
 	$adulte= fgets ( STDIN) ;
-	$r=intval(23%7) ;
-	$q=(23/7);
+	$r=intval($adulte%7) ;
+	$q=intval($adulte/7);
 	$prix = $q*7*15+$r*18;
 	echo " Prix a payer : $prix € " ;
 
